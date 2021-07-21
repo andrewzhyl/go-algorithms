e04a7fa1d84874f23b79da22b56b9d63b62ded4c
张玉龙 <andrew.zhyl@gmail.com>
2021-07-21 22:05:30 +0800

    add IterBinarySearch
    add InterpolationSearch

--- searchs/binary_search.go
+++ searchs/binary_search.go
@@ -1,17 +1,52 @@
 package searches
 
+// 二分查找
 func BinarySearch(array []int, key int) int {
 	low := 0
 	high := len(array) -1
 	for(low <= high){
 		mid := (low+high) / 2
-		if key < array[mid] {
-			high = mid -1
-		} else if(key > array[mid]){
-			low = mid + 1
-		} else {
+		if key == array[mid]{
 			return mid
 		}
+		if key < array[mid] {
+			high = mid -1
+		} else {
+			low = mid + 1
+		} 
+	}
+	return -1
+}
+
+// 递归二分查找
+func IterBinarySearch(array []int, key int, low int, high int) int {
+	if high < low || len(array) == 0 {
+		return -1
+	}
+	mid := (low + high) / 2
+	if key == array[mid]{
+		return mid
+	}
+	if key < array[mid] {
+		return IterBinarySearch(array, key, low, mid - 1)
+	} else {
+		return IterBinarySearch(array, key, mid + 1, high)
+	} 
+}
+
+func InterpolationSearch(array []int, key int) int {
+	low := 0
+	high := len(array) -1
+	for(low <= high){
+		mid := low + (high-low) * (key-array[low]) / (array[high] - array[low])
+		if key == array[mid]{
+			return mid
+		}
+		if key < array[mid] {
+			high = mid -1
+		} else {
+			low = mid + 1
+		} 
 	}
 	return -1
 }
--- searchs/search_test.go
+++ searchs/search_test.go
@@ -14,4 +14,22 @@ func TestBinarySearch(t *testing.T)  {
        fmt.Print(target)
        So(target, ShouldEqual, 7)
     })
+
+    Convey("#IterBinarySearch", t, func() {
+       items := []int{0,1,16,24,35,47,59,62,73,88,99}
+       target := IterBinarySearch(items, 62, 0, len(items) -1)
+       fmt.Printf("递归二分查找索引是: ")
+       fmt.Print(target)
+       So(target, ShouldEqual, 7)
+    })
+}
+
+func TestInterpolationSearch(t *testing.T)  {
+    Convey("#InterpolationSearch", t, func() {
+       items := []int{0,1,16,24,35,47,59,62,73,88,99}
+       target := BinarySearch(items, 62)
+       fmt.Printf("插值查找索引是: ")
+       fmt.Print(target)
+       So(target, ShouldEqual, 7)
+    })
 }
